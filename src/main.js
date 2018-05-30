84abeebf4fd60b321c7610cb6ff214c4e308f846
buccaneer33 <buccaneer33@inbox.ru>
2018-05-30 23:03:07 +0400

    server is done, let's make the page

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
+import './main.css';
 document.write('Hello, webpack');
-document.write(require('./content'));
 
-import'.main.css';
-import'.content.css';
-var _ = require('lodash');
-console.log(_.isEqual(1, 2));
